dc89a827c96e7927182ffc6e8c527babf15aee17
ammarqanoongo <98563313+ammarqanoongo@users.noreply.github.com>
2023-02-26 01:33:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "aws" {
   access_key = "your_access_key"
   secret_key = "your_secret_key"
-  region = "us-east-1"
+  region = "your_region"
   skip_credentials_validation = true
 }
 # please do not skip credential validation for this is just for my own personal use and practice 
